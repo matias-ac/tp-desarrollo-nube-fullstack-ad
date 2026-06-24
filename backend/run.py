03409917c87a82b966e18e927a6d065960699296
Matias Ariel Castro <matias.acastro1@gmail.com>
2026-06-24 16:51:05 -0300

    fix: mantener debug=True para desarrollo (127.0.0.1)

--- backend/run.py
+++ backend/run.py
@@ -4,4 +4,4 @@ app = create_app()
 
 if __name__ == "__main__":
     # Iniciamos el servidor en el puerto configurado, solo en localhost
-    app.run(host="127.0.0.1", port=app.config["PORT"], debug=False)
+    app.run(host="127.0.0.1", port=app.config["PORT"], debug=True)
